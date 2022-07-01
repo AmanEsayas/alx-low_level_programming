b29fda6f524fb31d189f193e5c4280bc3a04b2d2
Amani <amanuelesayas37@gmail.com>
2022-07-01 16:46:12 -0700

    task 9

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,14 +9,18 @@
 
 int main(void)
 {
-	char letter;
+	int num;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (num = 0; num <= 9; num++)
 	{
-		if (letter != 'e' && letter != 'q')
-			putchar(letter);
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		putchar(',');
+		putchar(',');
 	}
-	putchar('\n');
-	return (0);
 
+	putchar('\n');
+
+	return (0);
 }
