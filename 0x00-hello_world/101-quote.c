88b9b35cc6fe6ea847a3a14b6dd5842b41e2de63
Amani <amanuelesayas37@gmail.com>
2022-07-01 06:46:32 -0700

    ufff

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Return to 1
+ */
+int main(void)
+{
+	fwrite(2,"and that piece of art is useful - Dora korpar, 2015-10-19\n")
+	return (1);
+}
