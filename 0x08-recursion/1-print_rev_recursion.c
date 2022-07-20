0de524418d5fe6358cdb10cb775845aaf90461bf
Amani <amanuelesayas37@gmail.com>
2022-07-20 13:52:00 -0700

    task no 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,23 +1,15 @@
 #include "main.h"
 
 /**
- * _strncat - concatenates two strings upto n
- * @dest: destination string
- * @src: source string
- * @n: number of bytes to use from source
- *
- * Return: string dest pointed by a pointer
- */
-
-char *_strncat(char *dest, char *src, int n)
+* _print_rev_recursion -> printing string in reverse using recursion
+* @s: string s
+*/
+void _print_rev_recursion(char *s)
 {
-	int index = 0, dest_len = 0;
-
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
-
-	return (dest);
+	if (*s == '\0')
+		return;
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
 }
